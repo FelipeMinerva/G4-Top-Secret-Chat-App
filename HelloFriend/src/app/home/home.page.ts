58731c6e81b6a762eda3a67a08885917858df011
Felipe Minerva <felipe.minerva.42@gmail.com>
2019-07-25 23:48:00 -0300

    Pequenos testes de merge do git

--- HelloFriend/src/app/home/home.page.ts
+++ HelloFriend/src/app/home/home.page.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
   constructor() {}
+
+  test = true;
+
+  ngOnInit() {
+    this.test = !this.test;
+  }
 
 }
